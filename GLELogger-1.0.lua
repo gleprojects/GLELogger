98e2c5bda27ee20cdd28dcbbee0cf7dc1d72872b
Glenn Goslin <go.glenn@gmail.com>
2019-06-02 01:22:16 +0100

    add method to set log level

--- GLELogger-1.0.lua
+++ GLELogger-1.0.lua
@@ -15,38 +15,38 @@ local logLevels = {
 
 local defaults = {
     ['logLevel'] = 'WARN',
-        ['themes'] = {
-            ['ERROR'] = {
-                r = 1, --255/255
-                g = 0.3, -- 79/255
-                b = 0.2, -- 51/255
-                hex = "00ccff"
-            },
-            ['WARN'] = {
-                r = 1, -- 255/255
-                g = 0.6, -- 159/255
-                b = 0.2, -- 51/255
-                hex = "00ccff"
-            },
-            ['INFO'] = {
-                r = 0.2, -- 51/255
-                g = 0.2, -- 25/255
-                b = 0.8, --200/255
-                hex = "00ccff"
-            },
-            ['DEBUG'] = {
-                r = 0.2, -- 51/255
-                g = 0.7, -- 168/255
-                b = 1, --255/255
-                hex = "00ccff"
-            },
-            ['TRACE'] = {
-                r = 0.6,-- 147/255
-                g = 0.2, -- 51/255
-                b = 1, -- 255/255
-                hex = "00ccff"
-            }
+    ['themes'] = {
+        ['ERROR'] = {
+            r = 1, --255/255
+            g = 0.3, -- 79/255
+            b = 0.2, -- 51/255
+            hex = "00ccff"
+        },
+        ['WARN'] = {
+            r = 1, -- 255/255
+            g = 0.6, -- 159/255
+            b = 0.2, -- 51/255
+            hex = "00ccff"
+        },
+        ['INFO'] = {
+            r = 0.2, -- 51/255
+            g = 0.2, -- 25/255
+            b = 0.8, --200/255
+            hex = "00ccff"
+        },
+        ['DEBUG'] = {
+            r = 0.2, -- 51/255
+            g = 0.7, -- 168/255
+            b = 1, --255/255
+            hex = "00ccff"
+        },
+        ['TRACE'] = {
+            r = 0.6,-- 147/255
+            g = 0.2, -- 51/255
+            b = 1, -- 255/255
+            hex = "00ccff"
         }
+    }
 }
 
 local function getLoggingLevel(logLevel)
@@ -81,7 +81,11 @@ local function logMessage(src,msg,logLevelText)
 end
 
 function GLELogger:SetLogLevel(newLogLevel)
-    defaults['LogLevel'] = newLogLevel
+    defaults['logLevel'] = newLogLevel
+end
+
+function GLELogger:ShowLogLevel()
+    return defaults['logLevel']
 end
 
 function GLELogger:Debug(src,msg)
